59d7a925eda5583eb6b8e35a417fc8c9bc5fecd0
Sam Coll <scoll@sabiogroup.com>
2021-07-28 10:44:32 +0100

    Add URL page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,6 @@
 import { Box, Flex, Link } from "@chakra-ui/layout";
-import { Img } from "@chakra-ui/react";
+import { Link as ReachLink } from "@reach/router";
+import { Img, Spacer } from "@chakra-ui/react";
 import React from "react";
 
 interface NavBarProps {}
@@ -10,9 +11,17 @@ const NavBar: React.FC<NavBarProps> = ({}) => {
   return (
     <Flex zIndex={1} position="sticky" top={0} bg="#5E1BB3" p={4}>
       <Flex flex={1}>
-          <Box w="150px">
-            <Img src="./images/sabiologo.PNG" alt="Sabio Logo"/>
+          <Box w="150px">        
+            <Link href="/index">
+              <Img src="./images/sabiologo.PNG" alt="Sabio Logo"/>
+            </Link>
           </Box>
+          <Spacer></Spacer>
+          <Box w="150px" mt="6">
+            <Link href="/url" color="white">
+              URL Shorten
+            </Link>
+          </Box> 
         <Box ml={"auto"}>{body}</Box>
       </Flex>
     </Flex>
