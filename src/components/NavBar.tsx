ef148921cd50699f552d600cd006f94918e835ef
bantersmith <sam@bantersmith.com>
2021-07-29 15:26:03 +0100

    Changed navbar root link.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,22 +10,22 @@ const NavBar: React.FC<NavBarProps> = ({}) => {
   return (
     <Flex zIndex={1} position="sticky" top={0} bg="#5E1BB3" p={4}>
       <Flex flex={1}>
-          <Box w="150px">        
-            <Link href="/index">
-              <Img src="./images/sabiologo.PNG" alt="Sabio Logo"/>
-            </Link>
-          </Box>
-          <Spacer></Spacer>
-          <Box w="150px" mt="6">
-            <Link href="/url" color="white">
-              URL Shorten
-            </Link>
-          </Box>
-          <Box w="150px" mt="6">
-            <Link href="/topFive" color="white">
-              FAQ's
-            </Link>
-          </Box> 
+        <Box w="150px">
+          <Link href="/">
+            <Img src="./images/sabiologo.PNG" alt="Sabio Logo" />
+          </Link>
+        </Box>
+        <Spacer></Spacer>
+        <Box w="150px" mt="6">
+          <Link href="/url" color="white">
+            URL Shorten
+          </Link>
+        </Box>
+        <Box w="150px" mt="6">
+          <Link href="/topFive" color="white">
+            FAQ's
+          </Link>
+        </Box>
         <Box ml={"auto"}>{body}</Box>
       </Flex>
     </Flex>
