31cc73a10502da7a4268e6f8c55733f9879b7328
bantersmith <sam@bantersmith.com>
2021-07-30 07:50:03 +0100

    Remove un-used props

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,25 +1,18 @@
 import { Box, Spacer, Flex } from "@chakra-ui/layout";
 import React from "react";
-import {Img} from "@chakra-ui/react";
+import { Img } from "@chakra-ui/react";
 
-export const Wrapper: React.FC<WrapperProps> = ({
-  children,
-}) => {
+export const Wrapper: React.FC<any> = ({ children }) => {
   return (
     <Flex>
-      <Box 
-        mt={4}
-        mx={{base: "4", "md": "8"}}
-        maxW="1500px"
-        w="100%"
-      >
+      <Box mt={4} mx={{ base: "4", md: "8" }} maxW="1500px" w="100%">
         {children}
       </Box>
       <Img
         src="./images/robot2.PNG"
         boxSize="300px"
         alt="Sabio Logo"
-        d={{base: "none", "2xl": "block"}}
+        d={{ base: "none", "2xl": "block" }}
       />
     </Flex>
   );
