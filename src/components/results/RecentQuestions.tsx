f56e5fd693f1f5251081d1c8436d0e0d9eb7010e
Sam Coll <scoll@sabiogroup.com>
2021-07-30 09:18:20 +0100

    Add ask this functionality

--- src/components/results/RecentQuestions.tsx
+++ src/components/results/RecentQuestions.tsx
@@ -1,15 +1,16 @@
 import { Box, Button, Heading, Spacer, Stack, Text } from "@chakra-ui/react";
 import React from "react";
 
-const singleQuestion = (question) => {
+const singleQuestion = (question, onSubmit) => {
   return (
-    <Box pl={0} width="400px">
+    <Box pl={0} width="400px" key={question}>
       <Stack direction="row">
         <Text p="2" pl="0">
           {question}
         </Text>
         <Spacer />
         <Button
+          onClick={() => onSubmit({search: question})}
           size="xs"
           bgColor="#9FC6F6"
           textColor="#10006B"
@@ -21,9 +22,9 @@ const singleQuestion = (question) => {
   );
 };
 
-export const RecentQuestions: React.FC<any> = ({ questions }) => {
+export const RecentQuestions: React.FC<any> = ({ questions, onSubmit }) => {
   const renderedAnswers = questions.map((question) => {
-    return singleQuestion(question.friendlyName);
+    return singleQuestion(question.friendlyName, onSubmit);
   });
 
   return (
